6d6ea853c053ec7e0689fff23adda8310c324346
Pidan <chenyixiang_cyx@youzan.com>
2026-03-09 15:20:36 +0800

    fix: remove build next

--- examples/nextjs-demo/.next/static/chunks/app/layout-d655356d208de585.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{6150:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,1850,23))},1850:()=>{}},e=>{var s=s=>e(e.s=s);e.O(0,[989,906,299,358],()=>s(6150)),_N_E=e.O()}]);
